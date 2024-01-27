a239baadbc368c6bff53d03401f31e4ef544d66d
anna-fields <156122785+anna-fields@users.noreply.github.com>
2024-01-27 20:08:07 +0000

    added functional forecast

--- src/hw.js
+++ src/hw.js
@@ -15,8 +15,8 @@ humidityElement.innerHTML= `${response.data.temperature.humidity}% `;
 let windSpeedElement = document.querySelector("#windSpeed");
 windSpeedElement.innerHTML= `${response.data.wind.speed} km/h`;
 
+getForecast(response.data.city);
 
-console.log(response);
 }
 let searchForm = document.querySelector("#city-form");
 searchForm.addEventListener("submit", search);
@@ -42,32 +42,49 @@ axios.get(apiURL).then(displaySearch)
 
 fetchCurrentWeather("Sintra");
 
-
-
-function displayForecast (){
-  let forecast = document.querySelector("#forecast");
-
-let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-days.forEach (function (day){
-  forecastHtml = forecastHtml + ` <div class="weatherForecast">
-                <div class="raw">
-                    <div class="col2">
-                        <div class="forecastDate">${day}</div>
-                        <div class="forecastIcon"></div>
-                        <div class="forecastTemps">
-                            <span class="forecastMax"></span>
-                            <span class="forecastMin"></span>
-                        </div>
-                    </div>
-                </div>
-            </div>`;
-forecastElement.innerHTML=forecastHtml;})}
-
 function getForecast (city){
   let apiKey= "53980o1ef09d7553cet92b43aefbc155";
   let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios(apiURL).then(displayForecast);
 }
+
+function displayForecast (response){
+  let forecastHtml="";
+
+
+response.data.daily.forEach (function (day, index){
+  if(index<5){
+  forecastHtml = forecastHtml + `<div class="row">
+  <div class="col-2">
+   <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
+          </div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum)}º</div>
+        </div>
+      </div>
+      </div>
+      </div>`
+      ;
+          }
+}); 
+
+let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 
 
 
